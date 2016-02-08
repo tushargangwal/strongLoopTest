e6aac0ae4fb0d1d95a3c4d3c0792fb822befb479
root <root@ip-172-31-32-113.us-west-2.compute.internal>
2016-02-08 14:43:30 +0000

    Added testcase mocha

---
+++ test/hello-spec.js
@@ -0,0 +1,11 @@
+
+var expect = require('chai').expect;
+
+describe("Mocha tests", function(){
+
+  it("should say hello", function(){
+    var message = "Hello";
+    expect(message).to.equal("Hello");
+  })
+
+})
